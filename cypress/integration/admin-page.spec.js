ee381e16448b8eeb8f6dcb0fe16f50ae13dde70a
sehrish-bukhari <66178942+sehrish-bukhari@users.noreply.github.com>
2021-06-16 00:44:14 +0500

    commit 1

--- cypress/integration/admin-page.spec.js
+++ cypress/integration/admin-page.spec.js
@@ -1,5 +1,4 @@
 /// <reference types ="cypress"/>
-
 import { emailField, addAdmin, roleField, roleselector, saveButton, editAdmin, deleteAdmin, pageTab, addPage, pageName, readOnlyFeatures, selectReadOption, deleteReadOnlyFeatures, fullFeature, readOnlyFeaturesDropdown, fullFeatureDropdown, selectFullOption, addSubPage, editPage, savePage, deletepage, deleteFromEdit, redirectToAdminTab, navigate } from "../page-objects/add-admin"
 
 
@@ -7,23 +6,27 @@ describe("it should automate admin tab page", () => {
   // const adminTab = new adminTab()
 
   before(() => {         //call url function from cypress > fixture> url-file.json
-   navigate()                                        
-    })
-    
-  beforeEach(() => {         //call url function from cypress > fixture> url-file.json
-  redirectToAdminTab()                                         
+    navigate()
   })
 
-  it.only("should  add an admin", () => {
-   
-    addAdmin ()                                          //add admin from integration > page-object > add-admin.js,1st call addAdmin function
+  beforeEach(() => {
+    cy.login()
+    //call url function from cypress > fixture> url-file.json
+    redirectToAdminTab()
+  })
+
+
+
+  it("should  add an admin", () => {
+
+    addAdmin()                                          //add admin from integration > page-object > add-admin.js,1st call addAdmin function
     emailField('bsyedaserish@gmail.com')                 //then call a function for email field (enter email address)  
     roleField()                                           //then call a function that click on dropdown icon of the role field
     roleselector()                                         //then call a function that select the option from the dropdown
     saveButton()                                           //call a function that hit save button
   })
   it('should edit an admin', () => {
-    redirectToAdminTab()
+
     editAdmin()                                          //edit admin from integration > page-object > add-admin.js,1st call editAdmin function     
     roleField()                                         //then call a function that click on dropdown icon of the role field
     roleselector()                                      //then call a function that select the option from the dropdown
@@ -32,11 +35,13 @@ describe("it should automate admin tab page", () => {
   it('should delete an admin', () => {
     deleteAdmin()                                        //delete admin from integration > page-object > add-admin.js,call a function to delete admin
   })
-  it('should redirect to page tab', () => {
-    pageTab()                                     //add admin from integration > page-object > add-admin.js,call a function that redirect to the  page tab 
-  })
-  it('should add page', () => {
-    pageTab()                                        //add page from integration > page-object > add-admin.js,call a function that redirect to the page tab
+  // it('should redirect to page tab', () => {
+  //   pageTab()                                     //add admin from integration > page-object > add-admin.js,call a function that redirect to the  page tab 
+  // })
+  it('should click on add page', () => {
+
+    pageTab()
+   // cy.wait(20000)                                      //add page from integration > page-object > add-admin.js,call a function that redirect to the page tab
     addPage()                                         //call the function that click on add page button
     pageName('new page')                             //call a function that enter a page name
     readOnlyFeaturesDropdown()                        //call function the click on the dropdown of read only features 
@@ -47,6 +52,7 @@ describe("it should automate admin tab page", () => {
     addSubPage('sudofy')                             //call a function that click on subpage and ener the page name then hit save
 
   })
+
   it('should edit page', () => {
     pageTab()                                       //edit page from integration > page-object > add-admin.js,call a function that redirect to the page tab
     editPage()                                       //call a function that click on edit page
--- cypress/page-objects/add-admin.js
+++ cypress/page-objects/add-admin.js
@@ -1,23 +1,33 @@
 /// <reference types ="cypress"/>
 import '../fixtures/url-file.json'
 
+//********************************navigate to url********************************** */
 export function navigate(){
 cy.fixture('url-file').then(function (data) {
     this.data = data;    
     cy.visit(this.data[0].url)
-})}
-export function redirectToAdminTab(){
-  
-    cy.get('.MuiIconButton-label-104 > .material-icons').click()
  
-  cy.xpath("//span[contains(text(),'<Admin Roles>')]",{timeout:100000}).should('be.visible')
-    cy.xpath('').click()
+})}
+
+
+//********************************Admin role tab********************************** */
+export function redirectToAdminTab(){
+    cy.get('.MuiIconButton-label-104 > .material-icons',{timeout:700000}).should('be.visible')
+    cy.get('.MuiIconButton-label-104 > .material-icons').click()
+    //cy.scrollTo(0,200)
+    //cy.get('.ps__thumb-y').
+  cy.xpath('//body[1]/div[2]/div[3]/div[1]/div[1]/ul[1]/a[12]',{timeout:700000}).should('be.visible')
+    cy.xpath('//body[1]/div[2]/div[3]/div[1]/div[1]/ul[1]/a[12]').click({force:true})
 }
+
+//********************************click on add admin button********************************** */
 export function addAdmin() {
     cy.get('.p-8 > .MuiButtonBase-root > .MuiButton-label',{timeout:700000}).should('be.visible')
     cy.get('.p-8 > .MuiButtonBase-root > .MuiButton-label').click()
 
 }
+
+//********************************clicl on email field********************************** */
 export function emailField(emailFieldText) {
    // cy.get('#combo-box-demo', { timeout: 4000 }).should('be.visible')
     cy.get('#combo-box-demo').type(emailFieldText + '{enter}')
@@ -25,25 +35,38 @@ export function emailField(emailFieldText) {
     cy.get('#combo-box-demo-option-0').click()
 
 }
+
+//********************************clicl on ROLE field dropdown********************************** */
+
 export function roleField() {
     cy.xpath("//body/div[3]/div[3]/div[3]/div[1]/div[2]/div[1]/div[1]/div[1]/button[1]/span[1]/*[1]",{timeout:10000}).should('be.visible')
 
     cy.xpath("//body/div[3]/div[3]/div[3]/div[1]/div[2]/div[1]/div[1]/div[1]/button[1]/span[1]/*[1]").click()
 }
+
+//********************************clicl on role field********************************** */
+
 export function roleselector() {
     cy.get('#route_select-dropdown-option-4',{timeout:700000}).should('be.visible')
 
     cy.get('#route_select-dropdown-option-4').click()
 }
+
+//********************************clicl on save button of admin tab********************************** */
+
 export function saveButton() {
     cy.get('.justify-end > .MuiButtonBase-root > .MuiButton-label',{timeout:700000}).should('be.visible')
 
     cy.get('.justify-end > .MuiButtonBase-root > .MuiButton-label').click()
 }
+
+//********************************clicl on edit admin button********************************** */
 export function editAdmin() {
-    cy.get(':nth-child(5) > .justify-between > :nth-child(2) > :nth-child(1)',{timeout:600000}).should('be.visible')  
-      cy.get(':nth-child(21) > .justify-between > :nth-child(2) > :nth-child(1)').click()
+    cy.xpath('//body/div[@id="root"]/div[@id="fuse-layout"]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[2]/div[21]/div[1]/div[2]/span[1]',{timeout:700000}).should('be.visible')  
+      cy.xpath('//body/div[@id="root"]/div[@id="fuse-layout"]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[2]/div[21]/div[1]/div[2]/span[1]').click()
 }
+
+//********************************clicl on delete admin button********************************** */
 export function deleteAdmin() {
     cy.get(':nth-child(21) > .justify-between > :nth-child(2) > :nth-child(2)',{timeout:700000}).should('be.visible')
     cy.get(':nth-child(21) > .justify-between > :nth-child(2) > :nth-child(2)').click()
@@ -53,25 +76,39 @@ export function deleteAdmin() {
 }
 
 
+//********************************redirect to page tab********************************** */
 
 export function pageTab() {
     cy.get(':nth-child(3) > .MuiTab-wrapper',{timeout:700000}).should('be.visible')
     cy.get(':nth-child(3) > .MuiTab-wrapper').click()
 }
+
+//********************************clicl on add page button********************************** */
+
 export function addPage() {
-    cy.get('.p-8 > .MuiButtonBase-root > .MuiButton-label',{timeout:700000}).should('be.visible')
-    cy.get('.p-8 > .MuiButtonBase-root > .MuiButton-label').click()
+    cy.xpath('//body/div[@id="root"]/div[@id="fuse-layout"]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/button[1]',{timeout:700000}).should('be.visible')
+    cy.xpath('//body/div[@id="root"]/div[@id="fuse-layout"]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/button[1]').click()
 }
+
+//********************************page name********************************** */
+
 export function pageName(enterPageName) {
     cy.xpath('//body/div[3]/div[3]/div[3]/div[1]/div[1]/div[2]/div[1]/div[1]/input[1]').type(enterPageName + '{enter}')
 }
+
+//********************************click on read Only Features Dropdown********************************** */
+
 export function readOnlyFeaturesDropdown() {
-    cy.get('[role="combobox"][name="readOnlyFeatures"] > .MuiFormControl-root > .MuiInputBase-root > .MuiAutocomplete-endAdornment > .MuiButtonBase-root > .MuiIconButton-label > .MuiSvgIcon-root > path',{timeout:700000}).should('be.visible')
+    cy.get('[role="combobox"][name="readOnlyFeatures"] > .MuiFormControl-root > .MuiInputBase-root > .MuiAutocomplete-endAdornment > .MuiButtonBase-root > .MuiIconButton-label > .MuiSvgIcon-root > path',{timeout:100000}).should('be.visible')
     cy.get('[role="combobox"][name="readOnlyFeatures"] > .MuiFormControl-root > .MuiInputBase-root > .MuiAutocomplete-endAdornment > .MuiButtonBase-root > .MuiIconButton-label > .MuiSvgIcon-root > path').click()
 }
+
+//********************************select from read Only Features Dropdown********************************** */
+
 export function selectReadOption() {
     cy.get('#readOnlyFeatures-option-4',{timeout:700000}).should('be.visible')
-    cy.get('#readOnlyFeatures-option-4').click()
+     cy.get('#readOnlyFeatures-option-4').click()
+    
     readOnlyFeaturesDropdown()
     
     cy.get('#readOnlyFeatures-option-6',{timeout:700000}).should('be.visible')
@@ -81,18 +118,29 @@ export function selectReadOption() {
     cy.get('#readOnlyFeatures-option-13',{timeout:700000}).should('be.visible')
     cy.get('#readOnlyFeatures-option-13').click()
 }
+
+//********************************click on delete button of read Only Features Dropdown*********************** */
+
 export function deleteReadOnlyFeatures() {
     cy.get('[data-tag-index="0"] > .MuiSvgIcon-root',{timeout:700000}).should('be.visible')
     cy.get('[data-tag-index="0"] > .MuiSvgIcon-root').click()
 }
+//********************************click on full Features Dropdown*********************** */
+
 export function fullFeatureDropdown() {
     cy.get('[role="combobox"][name="allFeatures"] > .MuiFormControl-root > .MuiInputBase-root > .MuiAutocomplete-endAdornment > .MuiButtonBase-root > .MuiIconButton-label > .MuiSvgIcon-root > path',{timeout:700000}).should('be.visible')
     cy.get('[role="combobox"][name="allFeatures"] > .MuiFormControl-root > .MuiInputBase-root > .MuiAutocomplete-endAdornment > .MuiButtonBase-root > .MuiIconButton-label > .MuiSvgIcon-root > path').click()
 }
+
+//********************************delete From Edit********************** */
+
 export function deleteFromEdit() {
     cy.get('[role="combobox"][name="readOnlyFeatures"] > .MuiFormControl-root > .MuiInputBase-root > [data-tag-index="0"] > .MuiSvgIcon-root',{timeout:700000}).should('be.visible')
     cy.get('[role="combobox"][name="readOnlyFeatures"] > .MuiFormControl-root > .MuiInputBase-root > [data-tag-index="0"] > .MuiSvgIcon-root').click()
 }
+
+//********************************select Full feature Option(t********************** */
+
 export function selectFullOption() {
     cy.get('#allFeatures-option-4',{timeout:700000}).should('be.visible')
     cy.get('#allFeatures-option-4').click()
@@ -101,6 +149,9 @@ export function selectFullOption() {
     cy.get('#allFeatures-option-6',{timeout:700000}).should('be.visible')
     cy.get('#allFeatures-option-6').click()
 }
+
+//********************************add subpage********************** */
+
 export function addSubPage(subPageName) {
     cy.get(':nth-child(6) > :nth-child(1) > .MuiButton-label',{timeout:700000}).should('be.visible')
     cy.get(':nth-child(6) > :nth-child(1) > .MuiButton-label').click()
@@ -108,14 +159,23 @@ export function addSubPage(subPageName) {
     cy.get(':nth-child(7) > :nth-child(2) > .MuiButton-label',{timeout:700000}).should('be.visible')
     cy.get(':nth-child(7) > :nth-child(2) > .MuiButton-label').click()
 }
+
+//********************************Edit page********************** */
+
 export function editPage() {
     cy.get(':nth-child(40) > .items-center > div > :nth-child(1)',{timeout:700000}).should('be.visible');
     cy.get(':nth-child(40) > .items-center > div > :nth-child(1)').click()
 }
+
+//********************************save page button of page tab********************** */
+
 export function savePage() {
     cy.get(':nth-child(6) > :nth-child(2) > .MuiButton-label',{timeout:700000}).should('be.visible')
     cy.get(':nth-child(6) > :nth-child(2) > .MuiButton-label').click()
 }
+
+//********************************click on delete page********************** */
+
 export function deletepage() {
     cy.get(':nth-child(41) > .items-center > div > :nth-child(2)',{timeout:700000}).should('be.visible')
     cy.get(':nth-child(41) > .items-center > div > :nth-child(2)').click()
